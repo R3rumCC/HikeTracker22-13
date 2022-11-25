afd994868b7043f221a4482e313a08ca38c99fa8
LeonardoVolpini <volpini.leonardo00@gmail.com>
2022-11-25 12:22:51 +0100

    start hike unit test

--- server/unit_tests/hike.test.js
+++ server/unit_tests/hike.test.js
@@ -2,35 +2,20 @@
 
 const dao = require('../DAO');
 const testDao = require('../test-dao');
+import {rocciamelone} from './maps/rocciamelone';
+import {carborant} from './maps/Corborant-dal-buco-della-Marmotta';
 
-/*let mockGpx = `<?xml version='1.0' encoding='UTF-8' standalone='yes' ?>
-<gpx creator="www.flyisfun.com" version="1.1" xmlns="http://www.topografix.com/GPX/1/1" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd">
-  <trk>
-    <name>Track_n1</name>
-    <trkseg>
-      <trkpt lat="-48.843895" lon="10.9835696">
-        <ele>126.75549</ele>
-        <time>2016-04-16T11:05:00Z</time>
-      </trkpt>
-      <trkpt lat="-48.843254" lon="11.9823042">
-        <ele>126.90486</ele>
-        <time>2016-04-16T11:05:05Z</time>
-      </trkpt>
-    </trkseg>
-  </trk>
-</gpx>`
-*/
-
-/*describe("Hike test", () => {
+describe("Hike test", () => {
   beforeEach(async () => {
+    await testDao.run('DELETE FROME HikePoint');
+    await testDao.run('DELETE FROM Points');
     await testDao.run('DELETE FROM Hikes');
     // PROBLEM -> The insert doesn't go because the db need the gpx track
     await testDao.run("INSERT OR IGNORE INTO Hikes(title, length, expected_time, ascent, difficulty, start_point, end_point, reference_points, description, gpx_track)\
             VALUES ('Hike#1', '5.0', '5', '5.0', 'Tourist', '1', '2',\
-            '2-3', 'First easy example hike'), \
+            '2-3', 'First easy example hike', ?), \
             ('Hike#2', '10.0', '10', '10.0', 'Professional hiker', '3', '4',\
-            '4', 'Second example hike, very difficult')");
-    await testDao.run('DELETE FROM Points');
+            '4', 'Second example hike, very difficult, ?')",[rocciamelone, carborant]);
     await testDao.run("INSERT OR IGNORE INTO Points(address, nameLocation, gps_coordinates, type)\
                     VALUES ('La Riposa, GTA / 529 / SI, Trucco, Mompantero, Torino, Piedmont, 10059, Italy',\
                     'Hut#1', '45.177786,7.083372', 'Hut'), \
@@ -40,7 +25,6 @@ const testDao = require('../test-dao');
                     'Happy Parking Lot', '44.259583,7.039722', 'Parking Lot'), \
                     ('Vinadio, Cuneo, Piedmont, Italy',\
                     'Sad Parking Lot', '44.249216,7.017648', 'Parking Lot')");
-    await testDao.run('DELETE FROM HikePoint');
     await testDao.run("INSERT OR IGNORE INTO HikePoint(idPoint, titleHike)\
                     VALUES ('4', 'Hike#1'), \
                     ('3', 'Hike#2'), \
@@ -49,14 +33,15 @@ const testDao = require('../test-dao');
   });
 
   afterAll(async () => {
+    await testDao.run('DELETE FROME HikePoint');
+    await testDao.run('DELETE FROM Points');
     await testDao.run('DELETE FROM Hikes');
     // PROBLEM -> The insert doesn't go because the db need the gpx track
-    await testDao.run("INSERT OR IGNORE INTO Hikes(title, length, expected_time, ascent, difficulty, start_point, end_point, reference_points, description)\
+    await testDao.run("INSERT OR IGNORE INTO Hikes(title, length, expected_time, ascent, difficulty, start_point, end_point, reference_points, description, gpx_track)\
             VALUES ('Hike#1', '5.0', '5', '5.0', 'Tourist', '1', '2',\
-            '2-3', 'First easy example hike'), \
+            '2-3', 'First easy example hike', ?), \
             ('Hike#2', '10.0', '10', '10.0', 'Professional hiker', '3', '4',\
-            '4', 'Second example hike, very difficult')");
-    await testDao.run('DELETE FROM Points');
+            '4', 'Second example hike, very difficult, ?')",[rocciamelone, carborant]);
     await testDao.run("INSERT OR IGNORE INTO Points(address, nameLocation, gps_coordinates, type)\
                             VALUES ('La Riposa, GTA / 529 / SI, Trucco, Mompantero, Torino, Piedmont, 10059, Italy',\
                             'Hut#1', '45.177786,7.083372', 'Hut'), \
@@ -66,7 +51,6 @@ const testDao = require('../test-dao');
                             'Happy Parking Lot', '44.259583,7.039722', 'Parking Lot'), \
                             ('Vinadio, Cuneo, Piedmont, Italy',\
                             'Sad Parking Lot', '44.249216,7.017648', 'Parking Lot')");
-    await testDao.run('DELETE FROM HikePoint');
     await testDao.run("INSERT OR IGNORE INTO HikePoint(idPoint, titleHike)\
                                             VALUES ('4', 'Hike#1'), \
                                             ('3', 'Hike#2'), \
@@ -74,7 +58,7 @@ const testDao = require('../test-dao');
                                             ('1', 'Hike#1')");
   });
 
-  function Hike(title, length, expected_time, ascent, difficulty, start_point, end_point, reference_points, description) {
+  function Hike(title, length, expected_time, ascent, difficulty, start_point, end_point, reference_points, description, gpx_track) {
     this.title = title;
     this.length = length;
     this.expected_time = expected_time;
@@ -84,6 +68,7 @@ const testDao = require('../test-dao');
     this.end_point = end_point;
     this.reference_points = reference_points;
     this.description = description;
+    this.gpx_track = gpx_track;
   }
 
   function setStartPoint(hike, address, location, coordinates, type) {
@@ -98,42 +83,58 @@ const testDao = require('../test-dao');
     hike.end_point_nameLocation = location;
     hike.end_point_coordinates = coordinates;
     hike.end_point_type = type;
-  }       */
+  }       
 
   // PROBLEM -> Test doesn't go because yhe insert doesn't go
-  /*test('test readHikes', async () => {
+  test('test readHikes', async () => {
     const data = await dao.readHikes();
-    const hike1 = new Hike('Hike#1', '5.0', '5', '5.0', 'Tourist', '1', '2', '2-3', 'First easy example hike');
+    const hike1 = new Hike('Hike#1', '5.0', '5', '5.0', 'Tourist', '1', '2', '2-3', 'First easy example hike', rocciamelone);
     setStartPoint(hike1, "La Riposa, GTA / 529 / SI, Trucco, Mompantero, Torino, Piedmont, 10059, Italy",
       "Hut#1", "45.177786,7.083372", "Hut");
     setEndPoint(hike1, "Nostra Signora del Rocciamelone, 585, Novalesa, Torino, Piedmont, 10059, Italy",
       "Hut#2", "45.203531,7.07734", "Hut");
-    const hike2 = new Hike('Hike#2', '10.0', '10', '10.0', 'Professional hiker', '3', '4', '4', 'Second example hike, very difficult');
+    const hike2 = new Hike('Hike#2', '10.0', '10', '10.0', 'Professional hiker', '3', '4', '4', 'Second example hike, very difficult', carborant);
     setStartPoint(hike2, "327, Lago di San Bernolfo - Collalunga, Vinadio, Cuneo, Piedmont, Italy",
       "Happy Parking Lot", "44.259583,7.039722", "Parking Lot");
     setEndPoint(hike2, "Vinadio, Cuneo, Piedmont, Italy",
       "Sad Parking Lot", "44.249216,7.017648", "Parking Lot");
     const hikes_checks = [hike1, hike2];
     expect(data).toEqual(hikes_checks);
-  });*/
-  /*
+  });
+  
   test('test deleteHike', async () => {
     const data = await dao.readUsers();
-    const hike = new Hike('Hike#1', '5.0', '5', '5.0', 'Tourist', '1', '2', '2-3', 'First easy example hike');
+    const hike1 = new Hike('Hike#1', '5.0', '5', '5.0', 'Tourist', '1', '2', '2-3', 'First easy example hike', rocciamelone);
+    setStartPoint(hike1, "La Riposa, GTA / 529 / SI, Trucco, Mompantero, Torino, Piedmont, 10059, Italy",
+      "Hut#1", "45.177786,7.083372", "Hut");
+    setEndPoint(hike1, "Nostra Signora del Rocciamelone, 585, Novalesa, Torino, Piedmont, 10059, Italy",
+      "Hut#2", "45.203531,7.07734", "Hut");
+    const hike2 = new Hike('Hike#2', '10.0', '10', '10.0', 'Professional hiker', '3', '4', '4', 'Second example hike, very difficult', carborant);
+    setStartPoint(hike2, "327, Lago di San Bernolfo - Collalunga, Vinadio, Cuneo, Piedmont, Italy",
+        "Happy Parking Lot", "44.259583,7.039722", "Parking Lot");
+    setEndPoint(hike2, "Vinadio, Cuneo, Piedmont, Italy",
+        "Sad Parking Lot", "44.249216,7.017648", "Parking Lot");
+    const hikes_checks = [hike1, hike2];
+    expect(data).toEqual(hikes_checks);
     const check = await dao.deleteHike(hike.title);
     expect(check).toBe(true);
-  });*/
+    expect(data).not.toEqual(hikes_checks);
+  });
 
   //PROBLEM -> the db function doesn't have the field for gpx, but db wants it because gpx fields can't be null
-  /*test('test addHike', async () => {               
+  test('test addHike', async () => {  
+    await testDao.run('DELETE FROME HikePoint');             
     await testDao.run('DELETE FROM Hikes');
-    const hike = new Hike('Hike#1', '5.0', '5', '5.0', 'Tourist', '1', '2', '2-3', 'First easy example hike');
+    const hike = new Hike('Hike#1', '5.0', '5', '5.0', 'Tourist', '1', '2', '2-3', 'First easy example hike', rocciamelone);
     const check = await dao.addHike(hike);
     expect(check).toBe(true);
-  });*/
+    const data = await dao.readUsers();
+    const hikes_checks = [hike];
+    expect(data).toEqual(hikes_checks);
+  });
 
-  /*test('test readListOfReferencesPoints', async () => {
-    const hike = new Hike('Hike#1', '5.0', '5', '5.0', 'Tourist', '1', '2', '2-3', 'First easy example hike');
+  test('test readListOfReferencesPoints', async () => {
+    const hike = new Hike('Hike#1', '5.0', '5', '5.0', 'Tourist', '1', '2', '2-3', 'First easy example hike', rocciamelone);
     const ref = await dao.readListOfReferencePoints(hike.title);
     let refer_points = [];
     for (const rp of ref.reference_points.split("-")) {
@@ -143,12 +144,12 @@ const testDao = require('../test-dao');
     }
     const check = [2, 3];
     expect(check).toEqual(refer_points);
-  });*/
-/*
+  });
+
   test('test updateHike', async () => {               
-    const newHike = new Hike('Hike#5', '7.0', '5', '6.0', 'Tourist', '1', '2', '2-3-4', 'First easy example hike');
+    const newHike = new Hike('Hike#5', '7.0', '5', '6.0', 'Tourist', '1', '2', '2-3-4', 'First easy example hike', rocciamelone);
     const check = await dao.updateHike('Hike#1', newHike);
     expect(check).toBe(true);
   });
 
-});*/
+});
