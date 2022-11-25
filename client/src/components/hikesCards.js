5bcf616728bde85e36b76e7ac8ded5679812b3f9
R3rumCC <claudio.como05@gmail.com>
2022-11-25 14:14:33 +0100

    Hikes card improved

--- client/src/components/hikesCards.js
+++ client/src/components/hikesCards.js
@@ -5,16 +5,30 @@ import React, { Component }  from 'react';
 function HikeCard(props) {
     return(
         <Card className ="text-center me-2 my-1  " border="primary" style={{ width: '18rem' }}>
-            <Card.Header as="h5">{props.hike.title}</Card.Header>
+            <Card.Header as="h5"><strong>{props.hike.title}</strong></Card.Header>
             <Card.Body>
-                <Card.Text>Length: {props.hike.length}</Card.Text>
-                <Card.Text>Expected Time: {props.hike.expected_time}</Card.Text>
-                <Card.Text>Ascent: {props.hike.ascent}</Card.Text>
-                <Card.Text>Difficulty: {props.hike.difficulty}</Card.Text>
-                <Card.Text>Start Point: {props.hike.start_point_nameLocation}</Card.Text>
-                <Card.Text>End Point: {props.hike.end_point_nameLocation}</Card.Text>
-                {props.hike.reference_points.length > 0 ? <ListGroup>Reference Points:  {props.hike.reference_points.map((point)=>{return(<ListGroup.Item key={point.idPoint}>{point.nameLocation}</ListGroup.Item>)})}</ListGroup>: null}
-                <Card.Text>Description: {props.hike.description}</Card.Text>
+                <Row>
+                    <Col>
+                        <Card.Text><strong>Length:<br></br> {props.hike.length} km</strong></Card.Text>
+                    </Col>
+                    <Col>
+                        <Card.Text><strong>Expected Time:<br></br> {props.hike.expected_time} h</strong></Card.Text>
+                    </Col>
+                </Row>
+                <br></br>
+                <Row>
+                    <Col>
+                        <Card.Text><strong>Ascent:<br></br> {props.hike.ascent} m</strong></Card.Text>
+                    </Col>
+                    <Col>
+                        <Card.Text><strong>Difficulty:</strong><br></br><em>{props.hike.difficulty}</em></Card.Text>
+                    </Col>
+                </Row>
+                <br></br>
+                <Card.Text><strong>Start Point:</strong>{props.hike.start_point_nameLocation ? <><br></br><em>{props.hike.start_point_nameLocation}</em></> : null}<br></br>{props.hike.start_point_address}</Card.Text>
+                <Card.Text><strong>End Point:</strong>{props.hike.end_point_nameLocation ? <><br></br><em>{props.hike.end_point_nameLocation}</em></> : null}<br></br>{props.hike.end_point_address}</Card.Text>
+                {props.hike.reference_points.length > 0 ? <ListGroup><strong>Reference Points:</strong><br></br>  {props.hike.reference_points.map((point)=>{return(<ListGroup.Item key={point.idPoint}>{point.nameLocation}</ListGroup.Item>)})}</ListGroup>: null}
+                <Card.Text><strong>Description:</strong><br></br> {props.hike.description}</Card.Text>
                 {props.role=='Hiker' ? <Link to='/Map'><Button onClick={()=>{props.setCurrentHike([props.hike])}}>See on map</Button></Link>: null}
             </Card.Body>
         </Card>
